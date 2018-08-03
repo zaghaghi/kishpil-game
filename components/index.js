94d7ebc53de8bd413956fdcc9cb495cf60c63ac7
Hamed Zaghaghi <hamed.zaghaghi@gmail.com>
2018-08-03 18:36:31 +0430

    hide and reveal rects with no interaction

--- components/index.js
+++ components/index.js
@@ -6,10 +6,21 @@ export default class TouchableRect extends Component {
     state = {
         rerenders: 0
     }
+    revealTimer = null;
 
     render() {
         const playerName = this.props.text;
         const color = this._randomColor(this.props.colors);
+        if (this.props.revealAgain) {
+            this.revealTimer = setTimeout(() => {
+                this._view.zoomOut();
+                setTimeout(() => {
+                    this.setState({ rerenders: this.state.rerenders + 1 }, () => {
+                        this._view.zoomIn();
+                    })
+                }, Math.random() * 1000 + 500)
+            }, Math.random() * 1000 + 1500);
+        }
         return (
             <Animatable.View 
                 ref={(ref) => this._view = ref}
@@ -23,6 +34,9 @@ export default class TouchableRect extends Component {
                     this._view.zoomOut();
                     this.props.onPress(playerName, color);
                     if (this.props.revealAgain) {
+                        if (this.revealTimer) {
+                            clearTimeout(this.revealTimer);
+                        }
                         setTimeout(() => {
                             this.setState({ rerenders: this.state.rerenders + 1 }, () => {
                                 this._view.zoomIn();
